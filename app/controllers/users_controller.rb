377c704c899c77d2ed266ba50fe5f70235c65eb0
toda <cunmm602@gmail.com>
2022-12-04 13:38:06 +0000

    8章1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,31 @@
-def create
-  @user = User.new(user_params)
-  if @user.save
-    log_in @user # 保存成功後、ログインします。
-    flash[:success] = '新規作成に成功しました。'
-    redirect_to @user
-  else
-    render :new
+class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      flash[:success] = '新規作成に成功しました。'
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
