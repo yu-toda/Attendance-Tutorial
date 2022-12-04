13665e9562d12e944b20187fb0e1bef7eae63aa7
toda <cunmm602@gmail.com>
2022-12-04 01:58:27 +0000

    Finish login-function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,10 @@
-class UsersController < ApplicationController
-
-  def show
-    @user = User.find(params[:id])
+def create
+  @user = User.new(user_params)
+  if @user.save
+    log_in @user # 保存成功後、ログインします。
+    flash[:success] = '新規作成に成功しました。'
+    redirect_to @user
+  else
+    render :new
   end
-
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      flash[:success] = '新規作成に成功しました。'
-      redirect_to @user
-    else
-      render :new
-    end
-  end
-
-  private
-
-    def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    end
 end
