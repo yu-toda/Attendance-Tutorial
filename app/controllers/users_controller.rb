ed7b3fdd42ed8529755488b6b012a7bf208a97c9
toda <cunmm602@gmail.com>
2022-12-09 12:34:57 +0000

    8章3-1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,12 +47,13 @@ class UsersController < ApplicationController
     end
 
     # ログイン済みのユーザーか確認します。
-    def logged_in_user
-      unless logged_in?
-        flash[:danger] = "ログインしてください。"
-        redirect_to login_url
-      end
-    end
+   def logged_in_user
+    unless logged_in?
+    store_location
+    flash[:danger] = "ログインしてください。"
+    redirect_to login_url
+     end
+   end
 
     # アクセスしたユーザーが現在ログインしているユーザーか確認します。
     def correct_user
