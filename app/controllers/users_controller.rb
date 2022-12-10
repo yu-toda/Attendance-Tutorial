2ae6b6f2529931846b6f9daf1d1320ecfff8a91f
toda <cunmm602@gmail.com>
2022-12-10 03:03:29 +0000

    8章4終

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:show, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
 
+ def index
+  @users = User.paginate(page: params[:page])
+ end
+
   def show
   end
 
@@ -49,9 +53,9 @@ class UsersController < ApplicationController
     # ログイン済みのユーザーか確認します。
    def logged_in_user
     unless logged_in?
-    store_location
-    flash[:danger] = "ログインしてください。"
-    redirect_to login_url
+      store_location
+      flash[:danger] = "ログインしてください。"
+      redirect_to login_url
      end
    end
 
